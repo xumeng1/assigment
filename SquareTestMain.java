ae59e4b90c78cda792187449a366761f7c5553a3
xumeng1 <670397108@qq.com>
2015-07-21 17:56:39 +0800

    Create SquareTestMain.java

---
+++ SquareTestMain.java
@@ -0,0 +1,13 @@
+package  com;
+import java.util.Scanner;
+import com.Square;
+public class SquareTestMain {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Square s = new Square(20,"black");
+		 s.printInfo();
+	}
+}
