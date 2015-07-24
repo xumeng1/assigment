c1b56fdf741b4c1428f2354fb0969e7127cb1bfe
xumeng1 <670397108@qq.com>
2015-07-24 15:24:35 +0800

    Update Shape.java

--- 7-24/Shape/Shape.java
+++ 7-24/Shape/Shape.java
@@ -1,34 +1,5 @@
 package text;
-	public abstract  class Shape {
-		  double Area;
-		 double longRectangle;
-		 double wideRectangle;
-		 double Sidelength;	
-		 double AllArea;
-		public Shape(double longRectangle,double wideRectangle){
-			this.longRectangle=longRectangle;
-			this.wideRectangle=wideRectangle;
-		}
-		public Shape(double Sidelength){
-			this.Sidelength=Sidelength;
-		}
-		public double getlongRectangle(){
-			return longRectangle;
-		}
-		public double getwideRectangle(){
-			return wideRectangle;
-		}
-		public double getSidelength(){
-			return Sidelength;
-		}
-		public double getArea(){
-			return Area;
-		}		
+import java.util.ArrayList;
+public abstract  class Shape {
 	public abstract double CalculateArea();
-	public void AllArea( Shape c) {
-		AllArea+=c.Area;
-		System.out.println("总的面积为："+c.AllArea+"\n");	
-	}
-}
-	
-	
+}	
